1e5969361805c4886f50aa5de9e40b57c8b1082c
Lisa <alexanderlisa22@gmail.com>
2020-02-29 23:03:25 -0500

    headers added

--- contact-form/src/contactForm.js
+++ contact-form/src/contactForm.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
 
-//This code is functions and JSX elements
 function ContactForm() {
   const [email, setEmail] = useState();
 
@@ -17,7 +16,11 @@ function ContactForm() {
     
     fetch(url, {
       method: 'POST',
-      body: JSON.stringify({ email, firstName, lastName, message })
+      body: JSON.stringify({ email, firstName, lastName, message }),
+      headers: {
+        'Accept' : 'application/json',
+        'Content-Type': 'application/json'
+      },
     })
       .then(response => response.json())
       .then(response => console.log(response))
@@ -42,37 +45,32 @@ function ContactForm() {
 
 
 
-  //this is what is visible on the UI on the Contact page 
   return (
-    
+
 
     <form className="form" onSubmit={handleSubmit}>
 
-        
+
 
       <h1 className="container_form">Let's Connect</h1>
 
-      <label htmlFor="inputEmail" className="sr-only">Email address:</label>
-      <input onChange={handleEmailInput} type="email" id="inputEmail" className="form-control" placeholder="Add your Email" required autoFocus />
+      <label htmlFor="inputEmail" className="input">Email address:</label>
+      <input onChange={handleEmailInput} type="email" id="inputEmail" className="input" placeholder="Add your Email" required autoFocus />
 
 
-      <label htmlFor="inputFirstName" className="sr-only">First Name:</label>
-      <input onChange={handleFirstNameInput} type="text" id="inputFirstName" className="form-control" placeholder="First Name" required />
+      <label htmlFor="inputFirstName" className="input">First Name:</label>
+      <input onChange={handleFirstNameInput} type="text" id="inputFirstName" className="input" placeholder="First Name" required />
 
-      <label htmlFor="inputLastName" className="sr-only">Last Name:</label>
-      <input onChange={handleLastNameInput} type="text" id="inputLastName" className="form-control" placeholder="Last Name"  />
+      <label htmlFor="inputLastName" className="input">Last Name:</label>
+      <input onChange={handleLastNameInput} type="text" id="inputLastName" className="input" placeholder="Last Name" />
 
-      <label htmlFor="inputMessage" className="sr-only">Message:</label>
-      <textarea onChange={handleMessageInput} type="text" id="inputMessage" rows="3" cols= "30" className="form-control" placeholder="Leave a message..."  />
+      <label htmlFor="inputMessage" className="input">Message:</label>
+      <textarea onChange={handleMessageInput} type="text" id="inputMessage" rows="3" cols="30" className="input" placeholder="Leave a message..." />
 
 
-      <div className="checkbox mb-3">
-        <label>
-          <input type="checkbox" value="remember-me" /> Remember me
-        </label>
-      </div>
+
       <button className="btn" type="submit">Submit</button>
-     
+
     </form>
   );
 }
